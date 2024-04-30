48635cafc8499fa520e83974a4eba4798e953787
YillonMask <yixinrui24@gmail.com>
2024-04-30 10:56:59 -0700

    Update bubbleSort.c

--- sort/bubbleSort.c
+++ sort/bubbleSort.c
@@ -26,15 +26,14 @@ int main()
 {
     // assign a size of the array
     long size = 100;
-    int* numbers = (int*)malloc(size * sizeof(int));
+    int *numbers = (int *)malloc(size * sizeof(int));
     for (int i = 0; i < size; i++)
     {
         // assign random numbers from 0 to size of the array
         numbers[i] = rand() % size;
-
     }
     clock_t start_time = clock();
-    //bubbleSort(numbers, size);
+    // bubbleSort(numbers, size);
     clock_t end_time = clock();
     double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
     printf("total time spent during bubble sort is: %f\n", time_spent);
